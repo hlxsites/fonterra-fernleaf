6dc7f0b5fc2b61d4df96599c06f4f0bf03543f0f
Mohit Kumar <mohitkumar@adobe.com>
2023-07-25 10:36:08 +0530

    handled different combinations of hero banner

---
+++ blocks/hero/hero.js
@@ -0,0 +1,99 @@
+import {
+  isMobile,
+} from '../../scripts/scripts.js';
+
+const SECTION_BG_DESKTOP = 'bg-desktop';
+const SECTION_BG_MOBILE = 'bg-mobile';
+const SECTION_PRODUCT_DESKTOP = 'product-desktop';
+const SECTION_PRODUCT_MOBILE = 'product-mobile';
+const SECTION_CONTENT = 'content';
+let heroSections = [];
+
+function preProcess(block) {
+  heroSections = [];
+  [...block.children].forEach((row) => {
+    if (row.children[0].innerText === SECTION_CONTENT) {
+      heroSections[row.children[0].innerText] = row.children[1].innerHTML;
+    } else if (row.children.length === 2) {
+      heroSections[row.children[0].innerText] = row.children[1].children[0].children;
+    } else {
+      heroSections[SECTION_BG_DESKTOP] = row.children[0].children[0].children;
+      if (row.children[0].children.length === 2) {
+        heroSections[SECTION_CONTENT] = row.children[0].children[1].outerHTML;
+      }
+    }
+  });
+
+  return null;
+}
+
+function isValidImg(imgTag) {
+  if (heroSections[imgTag] && heroSections[imgTag].length === 4) {
+    return true;
+  }
+  return false;
+}
+
+function generateImage(imgDesktop, imgMobile, clsName) {
+  const dImg = heroSections[imgDesktop];
+  const mImg = heroSections[imgMobile];
+  const hImg = document.createElement('picture');
+  let imgTag = '';
+  [...dImg].forEach((item, index) => {
+    if (index !== 1 && index !== 3) {
+      hImg.append(item);
+    }
+    if (index === 3 && !isMobile()) {
+      imgTag = item;
+    }
+  });
+
+  [...mImg].forEach((item, index) => {
+    if (index === 1) {
+      hImg.append(item);
+    }
+    if (index === 3 && isMobile()) {
+      imgTag = item;
+    }
+  });
+  hImg.append(imgTag);
+
+  const imgContainer = document.createElement('div');
+  imgContainer.className = clsName;
+  imgContainer.append(hImg);
+  return imgContainer;
+}
+
+function generateSingleImage(imgDesktop, clsName) {
+  const dImg = heroSections[imgDesktop];
+  const hImg = document.createElement('picture');
+  [...dImg].forEach((item) => {
+    hImg.append(item);
+  });
+
+  const imgContainer = document.createElement('div');
+  imgContainer.className = clsName;
+  imgContainer.append(hImg);
+  return imgContainer;
+}
+
+export default function decorate(block) {
+  preProcess(block);
+  block.innerHTML = '';
+  if (isValidImg(SECTION_BG_DESKTOP) && isValidImg(SECTION_BG_MOBILE)) {
+    block.append(generateImage(SECTION_BG_DESKTOP, SECTION_BG_MOBILE, 'hero__bg'));
+  } else if (isValidImg(SECTION_BG_DESKTOP) && !isValidImg(SECTION_BG_MOBILE)) {
+    block.append(generateSingleImage(SECTION_BG_DESKTOP, 'hero__bg'));
+  }
+
+  if (isValidImg(SECTION_PRODUCT_DESKTOP) && isValidImg(SECTION_PRODUCT_MOBILE)) {
+    block.append(generateImage(SECTION_PRODUCT_DESKTOP, SECTION_PRODUCT_MOBILE, 'hero__product'));
+  }
+
+  if (heroSections[SECTION_CONTENT]) {
+    const divTag = document.createElement('div');
+    divTag.className = 'hero__description';
+    divTag.innerHTML = heroSections[SECTION_CONTENT];
+    block.append(divTag);
+  }
+}
