45f021f0f49a451a38f72e9264c890a739f6355b
Mohit Kumar <mohitkumar@adobe.com>
2023-07-28 11:00:16 +0530

    adding recipe list block

---
+++ blocks/recipe-list/recipe-list.js
@@ -0,0 +1,49 @@
+const getListHTML = (row) => `<div>
+            <a class="recipe-card" href="${row.path}" title="${row.shorttitle}">
+                <img src="${row.image}">
+                <div class="recipe-content">
+                    <h4>${row.shorttitle}</h4>
+                    <div class="recipe-desc">${row.shortdescription}</div>
+                    <div class="recipe-info">
+                        <div class="recipe-card-icon icon-icon-time">${row.duration}</div>
+                        <div class="recipe-card-icon icon-icon-user">${row.serves}</div>
+                    </div>
+                </div>
+            </a></div>`;
+
+async function loadData(path) {
+  if (path && path.startsWith('/')) {
+    const resp = await fetch(path);
+    const listData = JSON.parse(await resp.text());
+    // TODO: Sort data based on lastModified
+    return listData;
+  }
+  return null;
+}
+
+async function printList(list) {
+  const ul = document.createElement('ul');
+  list.data.forEach((row) => {
+    const li = document.createElement('li');
+    li.innerHTML = getListHTML(row);
+    ul.append(li);
+  });
+  return ul;
+}
+
+export default async function decorate(block) {
+  console.log('process started!');
+  const path = '/query-index.json?limit=500&offset=0&sheet=en-recipe';
+  const list = await loadData(path);
+  console.log('Printing response ... ');
+  console.log(list);
+
+  block.textContent = '';
+  if (list.data.length > 0) {
+    const objects = await printList(list);
+    block.append(objects);
+  } else {
+    block.append('no result found');
+  }
+  console.log('process finished!');
+}
