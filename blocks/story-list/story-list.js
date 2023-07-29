93572eb8746867190f11f95bc898effc484bd33e
Mohit Kumar <mohitkumar@adobe.com>
2023-07-29 20:12:15 +0530

    adding recipe list

---
+++ blocks/story-list/story-list.js
@@ -0,0 +1,42 @@
+import { getLanguage } from '../../scripts/scripts.js';
+
+const getListHTML = (row) => `<div class="story-image"><img alt="${row.shorttitle}" src="${row.image}"></div>
+            <div class="story-content">
+                <div class="story-title"><a href="${row.path}" title="${row.shorttitle}">${row.shorttitle}</a></div>
+                <p class="story-desc">${row.description}</p>
+                <a href="${row.path}" title="${row.shorttitle}" class="button primary">Read More</a>
+            </div>`;
+
+async function loadData(path) {
+  if (path && path.startsWith('/')) {
+    const resp = await fetch(path);
+    const listData = JSON.parse(await resp.text());
+    return listData;
+  }
+  return null;
+}
+
+async function printList(list) {
+  const ul = document.createElement('ul');
+  list.data.forEach((row) => {
+    const li = document.createElement('li');
+    li.classList.add('story');
+    li.innerHTML = getListHTML(row);
+
+    ul.append(li);
+  });
+  return ul;
+}
+
+export default async function decorate(block) {
+  const path = `/query-index.json?limit=500&offset=0&sheet=${getLanguage()}-story`;
+  const list = await loadData(path);
+
+  block.textContent = '';
+  if (list.data.length > 0) {
+    const objects = await printList(list);
+    block.append(objects);
+  } else {
+    block.append('no result found');
+  }
+}
