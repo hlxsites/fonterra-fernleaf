5f557a30109f1aa175c2eba7fadb101b02b524d3
Subash Selvaraj <sselvara@adobe.com>
2023-08-01 23:25:19 +0530

    update for adding serves and duration on recipe detail page

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -12,3 +12,46 @@ export async function isMobile() {
 
   return mql.matches;
 }
+
+// add serves and duration to recipe details page content
+const addServesAndDuration = {
+  content: (serves, duration) => {
+    const servesContent = serves
+      ? `<div class='serving'>
+        <span class='icon icon-icon-user'></span><span class='text'>${serves}</span>
+      </div>`
+      : '';
+    const durationContent = duration
+      ? `<div class='pre-time'>
+        <span class='icon icon-icon-time'></span>
+        <span class='text'>${duration}</span>
+      </div>`
+      : '';
+    return `<div class='recipe-info'>             
+              ${servesContent}
+              ${durationContent}
+              </div>`;
+  },
+  render: () => {
+    const servesMeta = document.querySelector("meta[name='serves']");
+    const serves = servesMeta ? servesMeta.getAttribute('content') : '';
+    const durationMeta = document.querySelector("meta[name='duration']");
+    const duration = durationMeta ? durationMeta.getAttribute('content') : '';
+    if (servesMeta || durationMeta) {
+      const recipeTitle = document.querySelector('h1');
+      recipeTitle.insertAdjacentHTML(
+        'afterend',
+        this.content(serves, duration),
+      );
+    }
+  },
+  init: () => {
+    const isRecipePage = document
+      .querySelector('body')
+      .classList.contains('recipe');
+    if (isRecipePage) {
+      this.render();
+    }
+  },
+};
+addServesAndDuration.init();
