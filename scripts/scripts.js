ecd68e27b15cc35d3e429754d24424652593db2e
Mohit Kumar <mohitkumar@adobe.com>
2023-08-04 20:27:51 +0530

    1. moved section bg logic to script.js. 2. set mobile image size to 750px

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -102,6 +102,64 @@ export function decorateLinkedPictures(container) {
     });
 }
 
+function GenerateBackGroundImages() {
+  this.addImageSource = (src, alt = '', eager = false, breakpoints = [{ media: '(min-width: 600px)', width: '1920' }, { width: '1023' }]) => {
+    if (breakpoints.length && src.length) {
+      const picture = document.createElement('picture');
+      const sourceElements = breakpoints.map((mediaPoints, index) => {
+        const { pathname } = new URL(src[index], window.location.href);
+        return `<source type='image/webp' ${mediaPoints.media ? `media='${mediaPoints.media}'` : ''} srcset='${pathname}?width=${mediaPoints.width}&format=webply&optimize=medium'>`;
+      });
+
+      const defaultIndex = 0;
+      const srcUrl = new URL(src[defaultIndex], window.location.href);
+      const sourcePathname = srcUrl?.pathname;
+      const ext = sourcePathname.substring(sourcePathname.lastIndexOf('.') + 1);
+      const fallbackSource = `<source ${breakpoints[defaultIndex].media ? `media='${breakpoints[defaultIndex].media}'` : ''} 
+                                      srcset='${sourcePathname}?width=${breakpoints[defaultIndex].width}&format=${ext}&optimize=medium'>`;
+      sourceElements.push(fallbackSource);
+
+      const defaultSrcIndex = breakpoints.length - 1;
+      const source = src[defaultSrcIndex] ? src[defaultSrcIndex] : src[0];
+      const imgUrl = new URL(source, window.location.href);
+      const imgPathname = imgUrl?.pathname;
+      const imgSrc = `<img src='${imgPathname}?width=${breakpoints[defaultSrcIndex].width}&format=${ext}&optimize=medium'
+                      alt='${alt}'
+                      width='${breakpoints[defaultSrcIndex].width}'
+                      height='100%'
+                      loading='${eager ? 'eager' : 'lazy'}'>
+                    `;
+      sourceElements.push(imgSrc);
+
+      const combinedElements = sourceElements.join('');
+      picture.innerHTML = combinedElements;
+      return picture;
+    }
+    return false;
+  };
+
+  this.render = (banner) => {
+    banner.forEach((elem) => {
+      const desktopBg = elem.dataset.backgroundDesktop;
+      const mobileBg = elem.dataset.backgroundMobile;
+      if (desktopBg && mobileBg) {
+        const responsiveImages = this.addImageSource([desktopBg, mobileBg], '', false, [{ media: '(min-width: 600px)', width: '1920' }, { width: '750' }]);
+        elem.append(responsiveImages);
+      } else {
+        elem.style.background = desktopBg || mobileBg;
+      }
+    });
+  };
+
+  this.init = () => {
+    const main = document.querySelector('main');
+    const banner = main.querySelectorAll('.full-width-banner');
+    if (banner) {
+      this.render(banner);
+    }
+  };
+}
+
 /**
  * Decorates the main element.
  * @param {Element} main The main element
@@ -114,6 +172,8 @@ export function decorateMain(main) {
   // buildAutoBlocks(main);
   decorateSections(main);
   decorateBlocks(main);
+
+  (new GenerateBackGroundImages()).init();
 }
 
 /**
