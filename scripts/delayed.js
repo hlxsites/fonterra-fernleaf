8a868e30fd891d2bea9bfed1c38cd6975ceab4a4
Subash Selvaraj <sselvara@adobe.com>
2023-08-10 22:01:22 +0530

    lint fixes

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -100,8 +100,10 @@ function UpdateExternalLinks() {
     const externalLinks = document.body.querySelectorAll('a');
     [...externalLinks].forEach((link) => {
       const linkHost = new URL(link.href).host;
-      // open the attribute in a new tab if it's not a javascript link and it's not a link to the current host
-      if(linkHost !== 'javascript:void(0)' && linkHost.indexOf("#") == 0 && linkHost !== currentHost) {
+      // open the attribute in a new tab if it's not a javascript link and
+      // it's not a link to the current host
+      // eslint-disable-next-line no-script-url
+      if ((linkHost !== 'javascript:void(0)') && (linkHost.indexOf('#') === 0) && (linkHost !== currentHost)) {
         link.setAttribute('target', '_blank');
         link.setAttribute('rel', 'noopener noreferrer');
       }
