1929301dc9e95c50aa20007520899ae3c64d38cf
ujjgupta <ujjgupta@adobe.com>
2023-08-16 18:49:40 +0530

    Adding animation, bullets to carousel. Hiding carousel for recipe desktop view.

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -4,10 +4,13 @@ import { fetchPlaceholders } from '../../scripts/lib-franklin.js';
 let current = 1;
 let numItems = 0;
 let category;
+let noOfBullets = 0;
+let bulletCurrent = 1;
+let interval;
 
 function next(event) {
-  const isRight = event.target.className === 'scroll-right';
-  const sliderList = event.target.parentElement.parentElement.getElementsByClassName(`${category}-slider-list`)[0];
+  const isRight = event === undefined || event.target.className === 'scroll-right';
+  const sliderList = document.querySelector('.slider-container');
   if (isRight) {
     sliderList.classList.add('slide-transition-right');
     sliderList.style.transform = 'translate3d(-25%, 0px,0px)';
@@ -17,13 +20,15 @@ function next(event) {
   }
 }
 
-function changeOrder(event) {
-  const sliderList = event.target.parentElement.parentElement.getElementsByClassName(`${category}-slider-list`)[0];
+function changeOrder() {
+  const sliderList = document.querySelector('.slider-container');
   const isRight = sliderList.classList.contains('slide-transition-right');
   if (isRight) {
     current = (current === numItems) ? 1 : current + 1;
+    bulletCurrent = (bulletCurrent === noOfBullets) ? 1 : bulletCurrent + 1;
   } else {
     current = (current === 1) ? numItems : current - 1;
+    bulletCurrent = (bulletCurrent === 1) ? noOfBullets : bulletCurrent - 1;
   }
 
   let order = 1;
@@ -37,57 +42,75 @@ function changeOrder(event) {
     order += 1;
   }
 
-  /* const e1 = document.querySelector('.bullet-active');
+  const e1 = document.querySelector('.bullet-active');
   e1.classList.remove('bullet-active');
-  const ele = document.getElementById(''+current);
-  ele.classList.add('bullet-active'); */
+  const ele = document.getElementById((((bulletCurrent - 1) % (noOfBullets)) + 1));
+  ele.classList.add('bullet-active');
 
   sliderList.classList.remove('slide-transition-right');
   sliderList.classList.remove('slide-transition-left');
-  sliderList.style.transform = 'translate3d(0px, 0px,0px)';
+  sliderList.style.transform = 'translate3d(0px, 0px, 0px)';
 }
 
-/*
-function createBullets() {
+function fetchItemsInCarousel() {
   const screenWidth = window.innerWidth;
-  console.log(`New Screen Width: ${screenWidth}px`);
-}
-
-const itemsInCarousel = screenWidth < 600 ? 2 : 3;
-for (let i = 1; i <= numItems - itemsInCarousel + 1; i += 1) {
-  const bullet = document.createElement('span');
-  if (i === 1) {
-    bullet.classList.add('bullet-active');
+  let itemsInCarousel = 0;
+  if (category === 'recipe') {
+    itemsInCarousel = screenWidth < 900 ? 2 : 3;
+  } else if (screenWidth < 600) {
+    itemsInCarousel = 2;
+  } else if (screenWidth < 900) {
+    itemsInCarousel = 3;
   } else {
-    bullet.classList.add('bullet');
+    itemsInCarousel = 4;
   }
-  bullet.id = i;
-  // block.append(bullet);
+  return itemsInCarousel;
+}
+
+function fetchNumberOfBullets() {
+  return (numItems - fetchItemsInCarousel() + 1);
+}
+
+function showStaticCarousel() {
+  if (numItems === fetchItemsInCarousel()) {
+    return true;
+  }
+  return false;
 }
 
 function createBullets() {
-  const screenWidth = window.innerWidth;
-  console.log(`Screen Width: ${screenWidth}px`);
-  const itemsInCarousel = screenWidth < 600 ? 2 : 3;
-
-  // Clear existing bullets before re-creating them
   const bulletsContainer = document.getElementById('bullets');
+  // Clear existing bullets before re-creating them
   bulletsContainer.innerHTML = '';
 
-  for (let i = 1; i <= numItems - itemsInCarousel + 1; i += 1) {
+  noOfBullets = fetchNumberOfBullets();
+  for (let i = 1; i <= noOfBullets; i += 1) {
     const bullet = document.createElement('span');
     if (i === 1) {
       bullet.classList.add('bullet-active');
-    } else {
-      bullet.classList.add('bullet');
     }
+    bullet.classList.add('bullet');
     bullet.id = i;
     bulletsContainer.appendChild(bullet);
   }
-
-  const sliderList = document.querySelector(`.${category}-slider-list`);
+  const sliderList = document.getElementById(`id-${category}-slider-list`);
   sliderList.append(bulletsContainer);
-} */
+}
+
+function showHideCarousel() {
+  const bulletsContainer = document.getElementById('bullets');
+  const leftButton = document.getElementById('id-scroll-left');
+  const rightButton = document.getElementById('id-scroll-right');
+  if (showStaticCarousel()) {
+    leftButton.style.display = 'none';
+    rightButton.style.display = 'none';
+    bulletsContainer.style.display = 'none';
+  } else {
+    leftButton.style.display = '';
+    rightButton.style.display = '';
+    bulletsContainer.style.display = '';
+  }
+}
 
 async function addCarouselHeader() {
   const placeholders = await fetchPlaceholders(`/${getLanguage()}`);
@@ -97,6 +120,21 @@ async function addCarouselHeader() {
   const headerText = category === 'recipe' ? placeholders.recipecarouselheading : placeholders.productscarouselheading;
   section.innerHTML = `<h3>${headerText}</h3>`;
   parentElement.insertBefore(section, parentElement.firstChild);
+}
+
+function autoScrollCarousel() {
+  const carouselWrapper = document.querySelector('.carousel-wrapper');
+  if (!showStaticCarousel() && interval === undefined) {
+    interval = setInterval(next, 3000);
+    // Stop the interval when the user interacts with the carousel
+    carouselWrapper.addEventListener('mouseenter', () => clearInterval(interval));
+    carouselWrapper.addEventListener('mouseleave', () => {
+      interval = setInterval(next, 3000); // Restart the interval when the user leaves
+    });
+  } else if (showStaticCarousel()) {
+    clearInterval(interval);
+    interval = undefined;
+  }
 }
 
 export default async function decorate(block) {
@@ -135,25 +173,28 @@ export default async function decorate(block) {
           <img src='${item.image}' alt='${item.image}'>
         </a>
         <h3>
-          <a href="${item.path}" title='${item.title}'>${item.title}</a>
+          <a href="${item.path}" title='${item.shorttitle}'>${item.shorttitle}</a>
         </h3>
       `;
 
       categoryContainer.appendChild(categoryElement);
     });
 
-    // -----------------
     const sliderList = document.createElement('div');
     sliderList.classList.add(`${category}-slider-list`);
-
-    const buttonl = document.createElement('button');
-    buttonl.classList.add('scroll-left');
-    buttonl.addEventListener('click', next, this);
-    block.append(buttonl);
+    sliderList.id = `id-${category}-slider-list`;
 
     const noOfItems = categoryContainer.children.length;
     numItems = noOfItems;
 
+    const buttonl = document.createElement('button');
+    buttonl.classList.add('scroll-left');
+    buttonl.id = 'id-scroll-left';
+    buttonl.addEventListener('click', next, this);
+    block.append(buttonl);
+
+    const sliderContainer = document.createElement('div');
+    sliderContainer.classList.add('slider-container');
     [...categoryContainer.children].forEach((row) => {
       const sliderItem = document.createElement('div');
       sliderItem.classList.add(`${category}-slider-item`);
@@ -161,33 +202,36 @@ export default async function decorate(block) {
       sliderItem.style.order = current;
       sliderItem.dataset.position = current;
       current += 1;
-      sliderList.append(sliderItem);
+      sliderContainer.append(sliderItem);
     });
+    sliderList.append(sliderContainer);
     block.append(sliderList);
-    current = 1;
 
-    /*
-    window.addEventListener('resize', createBullets);
-    // number of bullets = numItems - itemInCarousel + 1
-    // Initial call to create bullets
+    current = 1;
     const bulletsContainer = document.createElement('div');
     bulletsContainer.id = 'bullets';
-    const outerdiv = document.querySelector('.outerdiv');
-    outerdiv.append(bulletsContainer);
-    // block.append(bulletsContainer);
-    createBullets();
+    const carouselList = document.getElementById(`id-${category}-slider-list`);
+    carouselList.append(bulletsContainer);
 
-    // Listen for window resize
-    window.addEventListener('resize', createBullets); */
+    const carouselContainer = document.querySelector('.carousel-container');
+    const resizeObserver = new ResizeObserver(() => {
+      createBullets();
+    });
+    resizeObserver.observe(carouselContainer);
 
     const button = document.createElement('button');
     button.classList.add('scroll-right');
+    button.id = 'id-scroll-right';
     button.addEventListener('click', next, this);
     block.append(button);
 
-    block.querySelector(`.${category}-slider-list`).addEventListener('transitionend', (event) => {
-      changeOrder(event);
+    block.querySelector(`.${category}-slider-list`).addEventListener('transitionend', changeOrder);
+
+    const resizeObserverToShowOrHideCarousel = new ResizeObserver(() => {
+      showHideCarousel();
+      autoScrollCarousel();
     });
+    resizeObserverToShowOrHideCarousel.observe(carouselContainer);
   } else {
     block.append('no result found');
   }
