cee883d9253581a9be356f61afe29b0fdb9b59be
Subash Selvaraj <sselvara@adobe.com>
2023-08-31 19:30:03 +0530

    updated the card structure to suppot focus behaviour

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -17,10 +17,8 @@ export default function decorate(block) {
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
-    
     // wrap the card with the anchor link
     wrapCardwithLink(row, li);
-
     [...li.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
       else div.className = 'cards-card-body';
