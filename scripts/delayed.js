e9a88e05ef5f6a84802ca36dbf88a1bb42b61596
Mohit Kumar <mohitkumar@adobe.com>
2023-10-04 10:44:42 +0530

    updated GTM script with noscript tag

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -114,26 +114,12 @@ new UpdateExternalLinks().init();
 function GoogleTagManager() {
   this.loadGTM = () => {
     const scriptTag = document.createElement('script');
-    scriptTag.innerHTML = `
-    (function (w, d, s, l, i) {
-        w[l] = w[l] || [];
-        w[l].push({
-            'gtm.start':
-                new Date().getTime(), event: 'gtm.js'
-        });
-        var f = d.getElementsByTagName(s)[0],
-            j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
-        j.async = true;
-        j.src =
-            'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-        f.parentNode.insertBefore(j, f);
-    })(window, document, 'script', 'dataLayer', 'GTM-NX6JQZG');
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('set', {
-        'cookie_flags': 'SameSite=None;Secure'
-    });
-    `;
+    scriptTag.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-NX6JQZG');window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}gtag('set', { 'cookie_flags': 'SameSite=None;Secure' });`;
 
     const gtmIframe = document.createElement('iframe');
     gtmIframe.classList.add('gtm-iframe');
